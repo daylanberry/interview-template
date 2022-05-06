913a157c6ff40e6eb1ab00055d23914a9cd8373e
daberry_wh <daberry@williamhill.us>
2022-05-06 09:48:29 -0700

    add template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import DropdownGroup from "./components/DropdownGroup";
+
+//What is this line doing?
+export default function App() {
+  //what is the useState hook doing here?
+  const [dropdownOptions, setDropdownOptions] = useState({
+    sports: [
+      {
+        label: "Baseball",
+        value: "baseball",
+      },
+      {
+        label: "Football",
+        value: "football",
+      },
+      {
+        label: "Hockey",
+        value: "hockey",
+      },
+    ],
+    fruits: [
+      {
+        label: "Apple",
+        value: "apple",
+      },
+      {
+        label: "Orange",
+        value: "orange",
+      },
+      {
+        label: "Banana",
+        value: "banana",
+      },
+    ],
+    vegetables: [
+      {
+        label: "Broccoli",
+        value: "broccoli",
+      },
+      {
+        label: "Cauliflower",
+        value: "cauliflower",
+      },
+      {
+        label: "Zuchinni",
+        value: "zuchinni",
+      },
+    ],
+  });
+
+  const handleOptionSwitch = () => {
+    //write a function to replace the selected option in dropdown y with an object made
+    //from the text box value
+    //example dropdown x: fruits dropdown y: apple textbox: test
+    //result
+    /*    fruits: [
+      {
+        label: "test",
+        value: "test"
+      },
+      {
+        label: "Orange",
+        value: "orange"
+      },
+      {
+        label: "Banana",
+        value: "banana"
+      }
+    ],*/
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DropdownGroup
+      dropdownOptions={dropdownOptions}
+      handleOptionSwitch={handleOptionSwitch}
+    />
   );
 }
-
-export default App;
---
+++ src/components/DropdownGroup.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+// TODO
+// write two dropdown menus
+// the first dropdown menu will contain the titles  ex: (sports, fruits, vegetables)
+// the second dropdown will contain a list of categories which will be dynamically displayed depending on the selected title
+// example: if sports is selected in the first dropdown, then the second dropdown should have (Baseball, Football, Hockey) as options
+const DropdownGroup = ({ dropdownOptions = {}, handleOptionSwitch }) => {
+  //What are lines 21-23 doing? What do the empty strings mean?
+  const [replaceTerm, setReplaceTerm] = useState("");
+  const [firstDropdownOption, setFirstDropdownOption] = useState("");
+  const [secondDropdownOption, setSecondDropdownOption] = useState("");
+
+  return (
+    <div>
+      {Object.keys(dropdownOptions).map((option) => {
+        return (
+          <div>
+            <p>{option}</p>
+            <ul>
+              {dropdownOptions[option].map((op) => {
+                return <li> {op.label}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+      {/* todo: create a dropdown menu that contains the titles as options  ex: (sports, fruits, vegetables) */}
+
+      {/* todo: create a second dropdown menu that contains the selected title's options, ex: if sports is selected then (Baseball, Football, Hockey) are options    */}
+
+      {/* create a button with the text "replace" that has the click event handleOptionSwitch */}
+    </div>
+  );
+};
+
+//What is this line doing?
+export default DropdownGroup;
