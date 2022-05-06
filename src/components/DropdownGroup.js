f2a919e07165ff25a19f2c785e2357e9e2491865
daberry_wh <daberry@williamhill.us>
2022-05-06 12:29:19 -0700

    update todos

--- src/components/DropdownGroup.js
+++ src/components/DropdownGroup.js
@@ -19,16 +19,16 @@ const DropdownGroup = ({ dropdownOptions = {}, handleOptionSwitch }) => {
           </div>
         );
       })}
-      {/* todo: create a dropdown menu (dropdown x) that contains the titles as options  ex: (sports, fruits, vegetables) */}
-      {/* onChange updates firstDropdownOption */}
+      {/* todo: create a dropdown menu (dropdown x) that contains the dropdownOptions entries ex: (sports, fruits, vegetables) */}
+      {/* todo: onChange updates firstDropdownOption to selected entry */}
 
-      {/* todo: create a second dropdown menu (dropdown y) that contains the selected title's options, ex: if "sports" is selected then (Baseball, Football, Hockey) are options    */}
-      {/* todo: onChange updates firstDropdownOption */}
+      {/* todo: create a second dropdown menu (dropdown y) that contains the selected entry's labels, ex: if "sports" is selected then (Baseball, Football, Hockey) are options    */}
+      {/* todo: onChange updates secondDropdownOption label */}
 
-      {/* todo: create an input box with the placeholder "updated value" */}
+      {/* todo: under the second dropdown menu, create an input box with the placeholder "updated value" */}
       {/* todo: onChange updates replaceTerm */}
 
-      {/* create a button with the text "replace" that has the click event handleOptionSwitch */}
+      {/* todo: create a button with the text "replace" that has the click event handleOptionSwitch */}
     </div>
   );
 };
